9bc07b57d9ee47804786c2d9082ad319756c17c3
Scheldest-Audit <db-audit@github.com>
2026-06-29 05:23:47 +0000

    Added Game Controller Settings and Stability Controller

---
+++ app/src/main/java/com/winlator/xenvironment/XEnvironment.java
@@ -0,0 +1,80 @@
+package com.winlator.xenvironment;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
+import com.winlator.core.FileUtils;
+import com.winlator.xenvironment.components.GuestProgramLauncherComponent;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class XEnvironment implements Iterable<EnvironmentComponent> {
+    private final Context context;
+    private final ImageFs imageFs;
+    private final ArrayList<EnvironmentComponent> components = new ArrayList<>();
+
+    public XEnvironment(Context context, ImageFs imageFs) {
+        this.context = context;
+        this.imageFs = imageFs;
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public ImageFs getImageFs() {
+        return imageFs;
+    }
+
+    public void addComponent(EnvironmentComponent environmentComponent) {
+        environmentComponent.environment = this;
+        components.add(environmentComponent);
+    }
+
+    public <T extends EnvironmentComponent> T getComponent(Class<T> componentClass) {
+        for (EnvironmentComponent component : components) {
+            if (component.getClass() == componentClass) return (T)component;
+        }
+        return null;
+    }
+
+    @Override
+    public Iterator<EnvironmentComponent> iterator() {
+        return components.iterator();
+    }
+
+    public File getTmpDir() {
+        File tmpDir = new File(context.getFilesDir(), "tmp");
+        if (!tmpDir.isDirectory()) {
+            tmpDir.mkdirs();
+            FileUtils.chmod(tmpDir, 0771);
+        }
+        return tmpDir;
+    }
+
+    public void startEnvironmentComponents() {
+        FileUtils.clear(getTmpDir());
+        for (EnvironmentComponent environmentComponent : this) environmentComponent.start();
+    }
+
+    public void stopEnvironmentComponents() {
+        for (EnvironmentComponent environmentComponent : this) environmentComponent.stop();
+    }
+
+    public void onPause() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (preferences.getBoolean("stop_processes_on_suspend", true)) {
+            GuestProgramLauncherComponent guestProgramLauncherComponent = getComponent(GuestProgramLauncherComponent.class);
+            if (guestProgramLauncherComponent != null) guestProgramLauncherComponent.suspendProcess();
+        }
+    }
+
+    public void onResume() {
+        GuestProgramLauncherComponent guestProgramLauncherComponent = getComponent(GuestProgramLauncherComponent.class);
+        if (guestProgramLauncherComponent != null) guestProgramLauncherComponent.resumeProcess();
+    }
+}
